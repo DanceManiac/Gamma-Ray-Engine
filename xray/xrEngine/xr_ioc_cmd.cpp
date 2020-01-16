6b165b8e8b149f71e7f4c2c45260a546ae0c2d68
ign-1997 <gazelev6@yandex.ru>
2020-01-16 22:43:28 +0300

    Добавлена настройка hud_fov
    Вынесли настройку psHUD_FOV в консоль и меню.
    + необходимые .xml правки.
    Команда hud_fov [0.1-1.0]
    Также было добавлено отображение значения параметра, при регулировании
    ползунка в настройках.

--- xray/xrEngine/xr_ioc_cmd.cpp
+++ xray/xrEngine/xr_ioc_cmd.cpp
@@ -737,6 +737,7 @@ void CCC_Register()
 	CMD3(CCC_Mask,		"rs_refresh_60hz",		&psDeviceFlags,		rsRefresh60hz			);
 	CMD3(CCC_Mask,		"rs_stats",				&psDeviceFlags,		rsStatistic				);
 	CMD4(CCC_Float,		"rs_vis_distance",		&psVisDistance,		0.4f,	1.5f			);
+	CMD4(CCC_Float,		"hud_fov",				&psHUD_FOV,			0.45f,  1.8f			);
 
 	CMD3(CCC_Mask,		"rs_cam_pos",			&psDeviceFlags,		rsCameraPos				);
 #ifdef DEBUG
