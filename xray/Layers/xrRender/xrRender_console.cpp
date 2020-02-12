41a20e7f76763366f2421efa62cb17f0cbb119b4
ign-1997 <gazelev6@yandex.ru>
2020-02-12 20:00:08 +0300

    Screen Space Sunshafts из OGSE (Автор: KD87)
    + правки шейдеров.
    Перенастраивается командой r2_sunshafts_mode volumetric\screenspace.

--- xray/Layers/xrRender/xrRender_console.cpp
+++ xray/Layers/xrRender/xrRender_console.cpp
@@ -43,6 +43,14 @@ xr_token							qsun_shafts_token							[ ]={
 	{ 0,							0												}
 };
 
+//ogse sunshafts
+u32 ps_sunshafts_mode = 0;
+xr_token sunshafts_mode_token[] = {
+	{ "volumetric", 0 },
+	{ "screen_space", 1 },
+	{ nullptr, 0 }
+};
+
 u32			ps_r_ssao				=	3;
 xr_token							qssao_token									[ ]={
 	{ "st_opt_off",					0												},
@@ -250,6 +258,9 @@ float		dm_current_fade = 47.5;	//float(2*dm_current_size)-.5f;
 float		ps_current_detail_density = 0.6;
 float		ps_current_detail_scale = 1.f;
 
+//ogse sunshafts
+float		ps_r2_ss_sunshafts_length = 1.f;
+float		ps_r2_ss_sunshafts_radius = 1.f;
 
 Flags32 ps_actor_shadow_flags = { 0 };
 //- Mad Max
@@ -893,6 +904,11 @@ void		xrRender_initconsole	()
 //	float		ps_r2_dof_near			= 0.f;					// 0.f
 //	float		ps_r2_dof_focus			= 1.4f;					// 1.4f
 	
+	//ogse sunshafts
+	CMD3(CCC_Token, "r2_sunshafts_mode",				&ps_sunshafts_mode,			sunshafts_mode_token);
+	CMD4(CCC_Float, "r2_ss_sunshafts_length",			&ps_r2_ss_sunshafts_length, .2f, 1.5f);
+	CMD4(CCC_Float, "r2_ss_sunshafts_radius",			&ps_r2_ss_sunshafts_radius, .5f, 2.f);
+	//end ogse sunshafts 
 	CMD3(CCC_Mask,		"r2_volumetric_lights",			&ps_r2_ls_flags,			R2FLAG_VOLUMETRIC_LIGHTS);
 //	CMD3(CCC_Mask,		"r2_sun_shafts",				&ps_r2_ls_flags,			R2FLAG_SUN_SHAFTS);
 	CMD3(CCC_Token,		"r2_sun_shafts",				&ps_r_sun_shafts,			qsun_shafts_token);
